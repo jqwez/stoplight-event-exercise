79dc24ee09560441aaaf0cfc013afa479257c6f6
jqwez <77499225+jqwez@users.noreply.github.com>
2022-04-24 01:43:21 +0100

    Update index.js
    first strike at making this work

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-(function() {
-  'use strict';
 
-  // YOUR CODE HERE
-})();
+
+const greenButton = document.getElementById("goButton");
+const yellowButton = document.getElementById("slowButton");
+const redButton = document.getElementById("stopButton");
+
+let greenLight = document.getElementById("goLight");
+let yellowLight = document.getElementById("slowLight");
+let redLight = document.getElementById("stopLight");
+
+function toggle(light, color) {
+  if (light.style.background? "#111": false) {
+    light.style.backgroundColor = color;
+  } else {
+    light.style.backgroundColor = "#111";
+}
+  
+  greenButton.onclick = toggle(greenLight, "green")
+  yellowButton.onclick = toggle(yellowLight, "yellow")
+  redButton.onclick = toggle(redButton, "red")
